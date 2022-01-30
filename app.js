e35ca8d4d80a4d0b211d3538e6d05ad02a3e0ec1
Taylor Thomas K <taylorthomask@johndeere.com>
2022-01-29 23:42:16 -0600

    add do while

--- app.js
+++ app.js
@@ -15,14 +15,31 @@ do {
   guesses = this.getGuess();
   // if (isNaN(guesses) === false);
   console.log(guesses);
-  //this calls the getGuess function and saves it as a variable, guesses
+  
+//   var result = '';
+// var i = 0;
+// do {
+//    i += 1;
+//    result += i + ' ';
+// }
+// while (i > 0 && i < 5);
+// Despite i == 0 this will still loop as it starts off without the test
+
+console.log(result);
+
+
+
   this.prevGuesses.push(guesses);
   console.log(this.prevGuesses);
   this.render();
-  //this pops it into the prevGuesses array
+  // need to add while statement here
+  while (x = 0; i >= {
+
+
+  }
 }
 
-    console.log(prevGuesses);
+ //   console.log(prevGuesses);
   // const play = function () {
   //   if (this.biggestNum - this.smallestNum + 1)) + this.smallestNum);{
   //     return this.secretNum;
