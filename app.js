ffbb41d3a93130e9d36d9a7c4536875320f401c4
Taylor Thomas K <taylorthomask@johndeere.com>
2022-01-29 23:33:43 -0600

    add dp

--- app.js
+++ app.js
@@ -1,28 +1,35 @@
 console.log("I am here now")
 
 
-// const game = {
-//   title: 'Guess the Number!',
-//   biggestNum: 100,
-//   smallestNum: 1,
-//   secretNum: null,
-//   play: function() {
-//     this.secretNum = Math.floor(Math.random() * 
-//       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-//   }
-// }
+const game = {
+  title: 'Guess the Number!',
+  biggestNum: 100,
+  smallestNum: 1,
+  secretNum: null,
+  play: function() {
+    this.secretNum = Math.floor(Math.random() * 
+      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+  }
+}
+do {
+  guesses = this.getGuess();
+  // if (isNaN(guesses) === false);
+  console.log(guesses);
+  //this calls the getGuess function and saves it as a variable, guesses
+  this.prevGuesses.push(guesses);
+  console.log(this.prevGuesses);
+  this.render();
+  //this pops it into the prevGuesses array
+}
 
-
-//   const play = function (a, b, c) {
-//     if this.biggestNum - this.smallestNum + 1)) + this.smallestNum {
-//       return this.secretNum;
-//     // } else if (b >= a && b >= c) {
-//     //   return b;
-//     // }
-//     // return c;
-//   };
-//     console.log(play);
+    console.log(prevGuesses);
+  // const play = function () {
+  //   if (this.biggestNum - this.smallestNum + 1)) + this.smallestNum);{
+  //     return this.secretNum;
+  
+  // };
+  //   console.log("game");
   
   
 
-// // generate each for loop 
+// // generate each for loop }
