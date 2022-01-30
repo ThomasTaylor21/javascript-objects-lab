1f9013465cb111066eec7c2423e16b50a4f2d641
Taylor Thomas K <taylorthomask@johndeere.com>
2022-01-29 23:21:56 -0600

    retreating  to test

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 console.log("I am here now")
 
 
-const game = {
-  title: 'Guess the Number!',
-  biggestNum: 100,
-  smallestNum: 1,
-  secretNum: null,
-  play: function() {
-    this.secretNum = Math.floor(Math.random() * 
-      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-  }
-}
+// const game = {
+//   title: 'Guess the Number!',
+//   biggestNum: 100,
+//   smallestNum: 1,
+//   secretNum: null,
+//   play: function() {
+//     this.secretNum = Math.floor(Math.random() * 
+//       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+//   }
+// }
 
-prevGuesses: [], {
-  const play = function (a, b, c) {
-    if this.biggestNum - this.smallestNum + 1)) + this.smallestNum {
-      return this.secretNuma;
-    } else if (b >= a && b >= c) {
-      return b;
-    }
-    return c;
-  };
+
+//   const play = function (a, b, c) {
+//     if this.biggestNum - this.smallestNum + 1)) + this.smallestNum {
+//       return this.secretNum;
+//     // } else if (b >= a && b >= c) {
+//     //   return b;
+//     // }
+//     // return c;
+//   };
+//     console.log(play);
   
   
-  
-  for 
-// generate each for loop 
+
+// // generate each for loop 
