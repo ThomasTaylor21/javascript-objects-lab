cd44cd30254eaabee342913549207ab9b156bc53
Taylor Thomas K <taylorthomask@johndeere.com>
2022-02-01 01:09:57 -0600

    Solution

--- app.js
+++ app.js
@@ -2,6 +2,67 @@ console.log("I am here")
 
 
 
+   
+  console.log("test")
+
+  const game = {
+    title: 'Guess the Number!',
+    biggestNum: 10,
+    smallestNum: 1,
+    secretNum: null,
+    prevGuesses:[],
+  
+    getGuess:function(){
+      let guess
+      while (isNaN(guess) || guess < this.smallestNum || guess > this.biggestNum) {
+        
+        guess = prompt(`guess a number between ${this.smallestNum}  and ${this.biggestNum}`)
+        guess=parseInt(guess)
+        console.log(guess);
+      }
+      return guess
+    },
+  
+    play: function() {
+      
+      this.secretNum = Math.floor(Math.random() * 
+        (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
+        console.log(this.secretNum);
+      while (this.prevGuesses[this.prevGuesses.length - 1] !== this.secretNum) {
+          this.prevGuesses.push(this.getGuess())
+          console.log(this.prevGuesses);
+          if (this.prevGuesses[this.prevGuesses.length - 1] < this.secretNum) {
+            this.smallestNum = this.prevGuesses[this.prevGuesses.length - 1]
+          }else{
+            this.biggestNum = this.prevGuesses[this.prevGuesses.length - 1]
+          }
+          this.render();
+          
+      }
+    },
+  
+    render: function(){
+      if(this.prevGuesses[this.prevGuesses.length - 1] === this.secretNum){
+        alert(`Congrats ${this.prevGuesses.length}`)
+      }else{
+        alert(`guess is too ${this.prevGuesses[this.prevGuesses.length-1] > this.secretNum ? 'high' : 'low'}. prev guesses ${this.prevGuesses}`)
+      }
+  
+      // if(this.prevGuesses[this.prevGuesses.length - 1] < this.secretNum){      
+      //   alert(`too low prevGuesses: ${this.prevGuesses.join(', ')}`)
+      // }
+      // if(this.prevGuesses[this.prevGuesses.length - 1] > this.secretNum){      
+      //   alert(`too high prevGuesses: ${this.prevGuesses.join(', ')}`)
+      // }
+  
+    }
+  }
+  game.play()
+// note to GA Team......
+// I have the following, mostly copied from the given possible solution. I have traied a few tweaks to help understand the concepts. Credit goes to GA (Ben's work)
+
+
+
 // const game = {
 //   title: "Guess the Number!",
 //   biggestNum: null,
@@ -10,121 +71,106 @@ console.log("I am here")
 //   // Initialize prevGuesses as an array
 //   prevGuesses: [],
 //   play: function () {
-  const game = {
-    title: 'Guess the Number!',
-    biggestNum: 100,
-    smallestNum: 1,
-    secretNum: null,
-    play: function() {
-      this.secretNum = Math.floor(Math.random() * 
-        (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-    }
-  } 
-
-
-
-
-    this.setRange()
-    // Get the secretNum from the range
-    this.secretNum =
-      Math.floor(Math.random() * (this.biggestNum - this.smallestNum + 1)) +
-      this.smallestNum;
     
-    // 2. Add a `getGuess` method to `game` that prompts the player to enter a guess with a message formatted as: *Enter a guess between [smallestNum] and [biggestNum].*
-    // Hint - use a template literal for the prompt message.  // We want to run the game loop at least once, so a do...while loop works well here
-    do {
-      // Get the player's valid guess and push it into the prevGuesses array
-      this.prevGuesses.push(this.getGuess());
-      // BONUS: Call the resetRange method, reset the range after a guess is made
-      this.resetRange();
-      // Call the render method and render the result of the player's guess
-      this.render();
-      // If the player's last guess was not the secretNum, do it all again!
-    } while (this.prevGuesses[this.prevGuesses.length - 1] !== this.secretNum);
-  },
-  getGuess: function () {
-    // initialize a guess variable
-    let guess;
+//     this.setRange()
+//     // Get the secretNum from the range
+//     this.secretNum =
+//       Math.floor(Math.random() * (this.biggestNum - this.smallestNum + 1)) +
+//       this.smallestNum;
+//     // We want to run the game loop at least once, so a do...while loop works well here
+//     do {
+//       // Get the player's valid guess and push it into the prevGuesses array
+//       this.prevGuesses.push(this.getGuess());
+//       // BONUS: Call the resetRange method, reset the range after a guess is made
+//       this.resetRange();
+//       // Call the render method and render the result of the player's guess
+//       this.render();
+//       // If the player's last guess was not the secretNum, do it all again!
+//     } while (this.prevGuesses[this.prevGuesses.length - 1] !== this.secretNum);
+//   },
+//   getGuess: function () {
+//     // initialize a guess variable
+//     let guess;
 
-    do {
-      // Assign guess as an integer value parsed from the user's input to the prompt
-      guess = parseInt(
-        prompt(
-          `Enter a guess between ${this.smallestNum} and ${this.biggestNum}`
-        )
-      );
-    } while ( // While... 
-      isNaN(guess) || // the guess isn't a number or...
-      guess < this.smallestNum || // the guess is less than smallestNum or...
-      guess > this.biggestNum // the guess is greater than biggestNum
-    )
+//     do {
+//       // Assign guess as an integer value parsed from the user's input to the prompt
+//       guess = parseInt(
+//         prompt(
+//           `Enter a guess between ${this.smallestNum} and ${this.biggestNum}`
+//         )
+//       );
+//     } while ( // While... 
+//       isNaN(guess) || // the guess isn't a number or...
+//       guess < this.smallestNum || // the guess is less than smallestNum or...
+//       guess > this.biggestNum // the guess is greater than biggestNum
+//     )
 
-    // After we have exited the loop return the guess
-    return guess;
-  },
-  setRange: function() {
+//     // After we have exited the loop return the guess
+//     return guess;
+//   },
+//   setRange: function() {
 
-    do {
-      // Assign this.smallestNum as an integer value parsed from the user's input to the prompt
-      this.smallestNum = parseInt(
-        prompt(
-          `Enter a number - this will be the low end of the range.`
-        )
-      );
-    } while (isNaN(this.smallestNum)) //while this.smallestNum isn't a number
+//     do {
+//       // Assign this.smallestNum as an integer value parsed from the user's input to the prompt
+//       this.smallestNum = parseInt(
+//         prompt(
+//           `Enter a number - this will be the low end of the range.`
+//         )
+//       );
+//     } while (isNaN(this.smallestNum)) //while this.smallestNum isn't a number
 
-    do {
-      // Assign this.highestNum as an integer value parsed from the user's input to the prompt
-      this.biggestNum = parseInt(
-        prompt(
-          `Enter a number that is ${this.smallestNum + 2} or more. This will be the high end of the range.`
-        )
-      );
-    } while ( // While... 
-      isNaN(this.biggestNum) || // this.highestNum isn't a number
-      this.biggestNum < this.smallestNum + 2 // this.highestNum isn't larger than this.smallestNum + 2 (to allow space for this.secretNum to exist) 
-    )
-  },
-  // BONUS
-  resetRange: function () {
-    // If the last number the player entered is more than secretNum
-    if (this.prevGuesses[this.prevGuesses.length - 1] > this.secretNum) {
-      // set the biggestNum equal to the player's last guess
-      this.biggestNum = this.prevGuesses[this.prevGuesses.length - 1];
-    } else {
-      // otherwise set the smallestNum equal to the player's last guess
-      this.smallestNum = this.prevGuesses[this.prevGuesses.length - 1]
-    }
-  },
-  render: function () {
-    // initialize a msg variable
-    let msg;
+//     do {
+//       // Assign this.highestNum as an integer value parsed from the user's input to the prompt
+//       this.biggestNum = parseInt(
+//         prompt(
+//           `Enter a number that is ${this.smallestNum + 2} or more. This will be the high end of the range.`
+//         )
+//       );
+//     } while ( // While... 
+//       isNaN(this.biggestNum) || // this.highestNum isn't a number
+//       this.biggestNum < this.smallestNum + 2 // this.highestNum isn't larger than this.smallestNum + 2 (to allow space for this.secretNum to exist) 
+//     )
+//   },
+//   // BONUS
+//   resetRange: function () {
+//     // If the last number the player entered is more than secretNum
+//     if (this.prevGuesses[this.prevGuesses.length - 1] > this.secretNum) {
+//       // set the biggestNum equal to the player's last guess
+//       this.biggestNum = this.prevGuesses[this.prevGuesses.length - 1];
+//     } else {
+//       // otherwise set the smallestNum equal to the player's last guess
+//       this.smallestNum = this.prevGuesses[this.prevGuesses.length - 1]
+//     }
+//   },
+//   render: function () {
+//     // initialize a msg variable
+//     let msg;
 
-    // If the player has guessed the secretNum..
-    if (this.prevGuesses[this.prevGuesses.length - 1] === this.secretNum) {
-      // Assign the victory message to the msg variable
-      msg = `Congrats! You guessed the number in ${this.prevGuesses.length} ${
-        this.prevGuesses.length > 1 ? "guesses" : "guess"
-      }!`;
-    } else {
-      // otherwise assign the too high/low message to the msg variable
-      msg = `Your guess is too ${
-        this.prevGuesses[this.prevGuesses.length - 1] > this.secretNum
-          ? "high"
-          : "low"
-      }\nPrevious guesses: ${this.prevGuesses.join(", ")}`;
-    }
+//     // If the player has guessed the secretNum..
+//     if (this.prevGuesses[this.prevGuesses.length - 1] === this.secretNum) {
+//       // Assign the victory message to the msg variable
+//       msg = `Congrats! You guessed the number in ${this.prevGuesses.length} ${
+//         this.prevGuesses.length > 1 ? "guesses" : "guess"
+//       }!`;
+//     } else {
+//       // otherwise assign the too high/low message to the msg variable
+//       msg = `Your guess is too ${
+//         this.prevGuesses[this.prevGuesses.length - 1] > this.secretNum
+//           ? "high"
+//           : "low"
+//       }\nPrevious guesses: ${this.prevGuesses.join(", ")}`;
+//     }
 
-    // show the message in an alert dialog box
-    alert(msg);
-  },
-};
+//     // show the message in an alert dialog box
+//     alert(msg);
+//   },
+// };
 
-// call game.play and start the game!
-game.play();
+// // call game.play and start the game!
+// game.play();
 
 
-// const game = {
+// // const game = {
 //   title: 'Guess the Number!',
 //   biggestNum: 100,
 //   smallestNum: 1,
@@ -150,7 +196,8 @@ game.play();
 
 
 
-
+// 2. Add a `getGuess` method to `game` that prompts the player to enter a guess with a message formatted as: *Enter a guess between [smallestNum] and [biggestNum].*
+// Hint - use a template literal for the prompt message.
 // 3. Ensure that the `getGuess` method returns a value that:
 //     - Is a *number*, not a *string*.
 //     - Is between `smallestNum` and `biggestNum`, inclusive.
