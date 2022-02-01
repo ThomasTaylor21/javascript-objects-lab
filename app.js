c2fccc860ad2c78546724b5dd0eb70dcea3b4d8b
Taylor Thomas K <taylorthomask@johndeere.com>
2022-01-31 21:13:16 -0600

    add restart

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 console.log("I am here now")
 
-
 const game = {
   title: 'Guess the Number!',
-  biggestNum: 10,
+  biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
-  play: function () {
-    this.secretNum = Math.floor(Math.random() *
-    (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+  play: function() {
+    this.secretNum = Math.floor(Math.random() * 
+      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+  }
+}
   
     console.log(this.secretNum)
     do {
